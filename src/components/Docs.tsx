981e365c2fd68368aca164349efbad037bb67eb9
Alistair Smith <alistairsmith01@gmail.com>
2020-10-21 17:20:51 +0100

    Fix docs :whale::wheel_of_dharma:
    Signed-off-by: Alistair Smith <alistairsmith01@gmail.com>

--- src/components/Docs.tsx
+++ src/components/Docs.tsx
@@ -5,21 +5,12 @@ import { fetcher } from "../fetcher";
 import { Endpoint } from "./atoms/Endpoint";
 
 export const Docs = () => {
-  const { data, error } = useSWR<{ API_DOCS: ApiDocs }>(
+  const { data: body, error } = useSWR<{ data: ApiDocs }>(
     "https://otters.app/api/docs",
     fetcher
   );
 
-  if (!data?.API_DOCS) {
-    return (
-      <React.Fragment>
-        <br />
-        <h3>Loading</h3>
-      </React.Fragment>
-    );
-  }
-
-  if (error) {
+  if (error || !body) {
     return (
       <React.Fragment>
         <h1>Could not load docs!</h1>
@@ -28,13 +19,24 @@ export const Docs = () => {
     );
   }
 
-  const mappedEndpoints = Object.entries(data.API_DOCS.endpoints);
+  const { data = false } = body;
+
+  if (!data) {
+    return (
+      <React.Fragment>
+        <br />
+        <h3>Loading</h3>
+      </React.Fragment>
+    );
+  }
+
+  const mappedEndpoints = Object.entries(data.endpoints);
 
   return (
     <div>
       <br />
       <h3>
-        Base URL: <code>{data.API_DOCS.base_url}</code>
+        Base URL: <code>{data.base_url}</code>
       </h3>
       <div>
         {mappedEndpoints.map((entry) => {
